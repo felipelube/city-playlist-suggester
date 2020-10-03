1ead05909629e5a4f50b5a3b7904311b8d1f2deb
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2020-10-03 20:27:04 -0300

    Proof of concept do endpoint para busca por cidade [wip]

---
+++ src/Controller/CityPlaylistController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Services\LocationTemperatureGetter;
+use App\Services\MusicGenreChooser;
+use App\Services\SpotifyPlaylistGetter;
+
+class CityPlaylistController extends AbstractController
+{
+    private $musicGenreChooser;
+    private $locationTemperatureGetter;
+    private $playlistGetter;
+
+    public function __construct(
+        LocationTemperatureGetter $locationTemperatureGetter,
+        MusicGenreChooser $musicGenreChooser,
+        SpotifyPlaylistGetter $playlistGetter
+    ) {
+        $this->locationTemperatureGetter = $locationTemperatureGetter;
+        $this->musicGenreChooser = $musicGenreChooser;
+        $this->playlistGetter = $playlistGetter;
+    }
+
+    /**
+     * @Route("/cidade/{name}", name="city_playlist")
+     */
+    public function getPlaylistForCity($name)
+    {
+        $temperature = $this->locationTemperatureGetter->getTemperatureFromCityByName($name);
+        $genre = $this->musicGenreChooser::chooseGenreFromTemperature($temperature);
+        $playlist = $this->playlistGetter->getPlaylistForGenre($genre);
+        return $this->json([
+            'cidade' => $name,
+            'temperatura' => $temperature,
+            'gênero musical' => $genre,
+            'playlist' => $playlist
+        ]);
+    }
+}
