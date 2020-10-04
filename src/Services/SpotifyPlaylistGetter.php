d1b7e8ff3b55babd5060bc4b44ce076ef75b524d
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2020-10-04 13:18:02 -0300

    Não permita a criação de SpotifyPlaylistGetter sem as chaves da API

--- src/Services/SpotifyPlaylistGetter.php
+++ src/Services/SpotifyPlaylistGetter.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Exception\InvalidInputException;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
 use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
@@ -19,6 +20,14 @@ class SpotifyPlaylistGetter
 
     public function __construct(string $clientId, string $clientSecret, HttpClientInterface $httpClient, AdapterInterface $cache)
     {
+        if (!is_string($clientId) || empty($clientId)) {
+            throw new InvalidInputException('SpotifyPlaylistGetter: você precisa definir um clientId para usar este serviço.');
+        }
+
+        if (!is_string($clientSecret) || empty($clientSecret)) {
+            throw new InvalidInputException('SpotifyPlaylistGetter: você precisa definir um clientSecret para usar este serviço.');
+        }
+
         $this->clientId = $clientId;
         $this->clientSecret = $clientSecret;
         $this->httpClient = $httpClient;
@@ -29,13 +38,13 @@ class SpotifyPlaylistGetter
     {
         $authorizationValue = \base64_encode("{$this->clientId}:{$this->clientSecret}");
         $response = $this->httpClient->request('POST', self::SPOTIFY_ACCESS_TOKEN_ENDPOINT, [
-      'body' => [
-        'grant_type' => self::SPOTIFY_GRANT_TYPE,
-      ],
-      'headers' => [
-        'Authorization' => "Basic {$authorizationValue}",
-      ], //TODO: fazer método para criar essa requisição autorizada
-    ]);
+                'body' => [
+                'grant_type' => self::SPOTIFY_GRANT_TYPE,
+            ],
+            'headers' => [
+                'Authorization' => "Basic {$authorizationValue}",
+            ], //TODO: fazer método para criar essa requisição autorizada
+        ]);
 
         return $response;
     }
@@ -55,14 +64,14 @@ class SpotifyPlaylistGetter
         // TODO: usar uma resposta cacheada apenas como último recurso ou reduzir o TTL do cache
         return $this->cache->get("genre-$genre", function (ItemInterface $item) use ($genre) {
             $response = $this->httpClient->request('GET', self::SPOTIFY_RECOMMENDATIONS_ENDPOINT, [
-        'headers' => [
-          'Authorization' => "Bearer {$this->getAccessToken()}",
-        ],
-        'query' => [
-          'seed_genres' => $genre,
-          'target_popularity' => 70,
-        ],
-      ])->toArray();
+                'headers' => [
+                'Authorization' => "Bearer {$this->getAccessToken()}",
+                ],
+                'query' => [
+                'seed_genres' => $genre,
+                'target_popularity' => 70,
+                ],
+            ])->toArray();
             $item->expiresAfter(3600); //TODO: parametrizar
 
             return array_map(function ($track) {
